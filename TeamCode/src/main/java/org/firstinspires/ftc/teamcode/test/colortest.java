caa703d348f133096fcb15c0e6ac44b14de77360
CrazySable <hinmanc2008@gmail.com>
2026-04-19 19:11:42 -0400

    4/19 color sensor test update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/colortest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/colortest.java
@@ -2,24 +2,35 @@ package org.firstinspires.ftc.teamcode.test;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.LED;
 
 import org.firstinspires.ftc.teamcode.Components.Colors;
 
 import org.firstinspires.ftc.teamcode.GorillabotCentral;
 
 
+
 @TeleOp
 @Config
 public class colortest extends GorillabotCentral {
 
+
     @Override
     public void runOpMode() throws InterruptedException {
 
-        double artifact_detected = 0;
-        Colors.DetectedColor prev1 = Colors.DetectedColor.EMPTY;
-        Colors.DetectedColor prev2 = Colors.DetectedColor.EMPTY;
-        Colors.DetectedColor prev3 = Colors.DetectedColor.EMPTY;
 
+        DigitalChannel led1;
+        led1 = hardwareMap.get(DigitalChannel.class, "led1");
+        led1.setMode(DigitalChannel.Mode.OUTPUT);
+
+        DigitalChannel led2;
+        led2 = hardwareMap.get(DigitalChannel.class, "led2");
+        led2.setMode(DigitalChannel.Mode.OUTPUT);
+
+        DigitalChannel led3;
+        led3 = hardwareMap.get(DigitalChannel.class, "led3");
+        led3.setMode(DigitalChannel.Mode.OUTPUT);
 
         waitForStart();
         initializeComponents();
@@ -29,53 +40,38 @@ public class colortest extends GorillabotCentral {
             Colors.DetectedColor slot2 = colors.getColor2();
             Colors.DetectedColor slot3 = colors.getColor3();
 
-
-            if(prev1 == Colors.DetectedColor.EMPTY && slot1 == Colors.DetectedColor.BALL) {
-                artifact_detected++;
-                prev1 = slot1;
+            if(slot1 == Colors.DetectedColor.BALL){
+                led1.setState(false);
+            }
+            else if(slot1 == Colors.DetectedColor.EMPTY){
+                led1.setState(true);
             }
 
-            if(prev2 == Colors.DetectedColor.EMPTY && slot2 == Colors.DetectedColor.BALL){
-                artifact_detected ++;
-                prev2 = slot2;
+            if(slot2 == Colors.DetectedColor.BALL){
+                led2.setState(false);
+            }
+            else if(slot2 == Colors.DetectedColor.EMPTY){
+                led2.setState(true);
             }
 
-            if(prev3 == Colors.DetectedColor.EMPTY && slot3 == Colors.DetectedColor.BALL){
-                artifact_detected ++;
-                prev3 = slot3;
+            if(slot3 == Colors.DetectedColor.BALL){
+                led3.setState(false);
             }
-
-            if(prev1 == Colors.DetectedColor.BALL && slot1 == Colors.DetectedColor.EMPTY) {
-                artifact_detected--;
-                prev1 = slot1;
-            }
-
-            if(prev2 == Colors.DetectedColor.BALL && slot2 == Colors.DetectedColor.EMPTY){
-                artifact_detected --;
-                prev2 = slot2;
-            }
-
-            if(prev3 == Colors.DetectedColor.BALL && slot3 == Colors.DetectedColor.EMPTY){
-                artifact_detected --;
-                prev3 = slot3;
+            else if(slot3 == Colors.DetectedColor.EMPTY){
+                led3.setState(true);
             }
 
 
 
 
-            telemetry.addData("artifacts", artifact_detected);
+
             telemetry.addData("sensor 1", colors.getColor1());
             telemetry.addData("sensor 2", colors.getColor2());
             telemetry.addData("sensor 3", colors.getColor3());
             telemetry.addData("red", colors.color1.red());
-            telemetry.addData("blue", colors.color1.blue());
-            telemetry.addData("green", colors.color1.green());
             telemetry.update();
 
         }
-
-
-
 
 
 
